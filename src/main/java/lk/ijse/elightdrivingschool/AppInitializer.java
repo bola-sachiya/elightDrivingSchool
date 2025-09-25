0c14c150767644702b70c0d41da3acaeb59fc73c
Sachintha Dewmin <sachinthadewmina@gmail.com>
2025-09-25 20:02:48 +0530

    Final

--- src/main/java/lk/ijse/elightdrivingschool/AppInitializer.java
+++ src/main/java/lk/ijse/elightdrivingschool/AppInitializer.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class AppInitializer extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Elite Driving School System");
